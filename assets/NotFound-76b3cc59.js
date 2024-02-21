2c638f85e1f70aa43da4a3852c6076b335dd9f48
onexez <wowqet@mail.ru>
2024-02-21 19:14:35 +0300

    Updates

--- assets/NotFound-76b3cc59.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-4aa30efc.js";const t="_root_maqmx_1",s={root:t},r=()=>o.jsx("h1",{className:s.root,children:"Нечего не найдено"}),c=()=>o.jsx(r,{});export{c as default};
---
+++ assets/NotFound-9fcf11ac.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-e6454b28.js";const t="_root_maqmx_1",s={root:t},r=()=>o.jsx("h1",{className:s.root,children:"Нечего не найдено"}),c=()=>o.jsx(r,{});export{c as default};
